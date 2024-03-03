c940777406364e5eb2be539bd21e11d27eed71ed
Blaster4385 <blaster4385@tablaster.dev>
2024-03-03 18:17:27 +0530

    feat: added support for two monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,4 +2,6 @@
 
 killall -q polybar
 
-polybar --config=$HOME/.config/polybar/config.ini top
+MONITOR=DP-2 polybar --config=$HOME/.config/polybar/config.ini top
+sleep 1
+MONITOR=DP-0 polybar --config=$HOME/.config/polybar/config.ini top
