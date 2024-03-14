edb4fbac3cde9d8c06a8e9e6ad27f543f0a75903
Blaster4385 <blaster4385@tablaster.dev>
2024-03-14 23:37:47 +0530

    chore: install font packages

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yay -S alacritty conky dunst i3-wm i3lock mpd polybar rofi gtk-engine-murrine
+yay -S alacritty conky dunst i3-wm i3lock mpd polybar rofi gtk-engine-murrine ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts ttf-font-awesome neovim 
