354496635ea960960d43dfeab8eee9d8341edd89
Blaster4385 <venkatesh@tablaster.dev>
2024-12-01 00:08:36 +0530

    feat: added neomutt config

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yay -S alacritty conky dunst i3-wm i3lock mpd polybar rofi gtk-engine-murrine ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts ttf-font-awesome neovim 
+yay -S alacritty conky dunst i3-wm i3lock mpd neomutt polybar rofi gtk-engine-murrine ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts ttf-font-awesome neovim 
