61a4571c81acd012dba37a5b19247f799663bb79
Blaster4385 <venkatesh@tablaster.dev>
2024-12-01 00:12:56 +0530

    feat: updated nvim conform config

--- nvim/custom/configs/conform.lua
+++ nvim/custom/configs/conform.lua
@@ -1,36 +1,16 @@
---type conform.options
-local util = require("conform.util")
-
-local options = {
-  lsp_fallback = true,
-
-  formatters_by_ft = {
-    lua = { "stylua" },
-
-    javascript = { "prettier", "eslint" },
-    javascriptreact = { "prettier", "eslint" },
-    css = { "prettier" },
-    html = { "prettier" },
-
-    sh = { "shfmt" },
-  },
-
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 1000,
-    lsp_fallback = true,
-  },
-
-  formatters = {
-    eslint = {
-      command = util.from_node_modules("eslint"),
-      args = { "$FILENAME", "--fix" },
-      cwd = util.root_file({
-        "package.json",
-        ".eslintrc.cjs"
-      }),
-    }
-  }
-}
-
-require("conform").setup(options)
+require("conform").setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      javascriptreact = { { "prettierd", "prettier" } },
+      html = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+  })
