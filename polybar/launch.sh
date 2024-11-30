1f825cbc5e545f1e3df14af2b7b0d244e930c764
Blaster4385 <venkatesh@tablaster.dev>
2024-12-01 00:02:40 +0530

    feat: updated polybar config
    - Update cpu temp path
    - Update monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killall -q polybar
-
+# sleep 1
+MONITOR=DP-4 polybar --config=$HOME/.config/polybar/config.ini top
+sleep 2
 MONITOR=DP-2 polybar --config=$HOME/.config/polybar/config.ini top
-sleep 1
-MONITOR=DP-0 polybar --config=$HOME/.config/polybar/config.ini top
